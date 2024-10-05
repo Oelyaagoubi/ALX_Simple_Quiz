3c465daca37a23bdfad4ca9f184bd6065104855a
Oualid Elyaagoubi <oualidelyaagoubi@ouatlook.com>
2024-10-05 13:57:25 +0100

    done

---
+++ quiz.js
@@ -0,0 +1,12 @@
+function checkAnswer(){
+    let correctAnswer = "4";
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+    if(correctAnswer == userAnswer){
+        document.getElementById("feedback").innerHTML = 'Correct! Well done.';
+    }else if(correctAnswer !== userAnswer){
+        document.getElementById("feedback").innerHTML = "That's incorrect. Try again!";
+    }
+}
+
+const buttonCheck = document.getElementById("submit-answer");
+buttonCheck.addEventListener("click", checkAnswer);
